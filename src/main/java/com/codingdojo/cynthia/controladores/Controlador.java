d716ca0b28605e70873dddefc9bc033cac3b87c2
Cynthia Castillo <ca.castillo@gmail.com>
2023-04-21 20:02:12 -0600

    Fecha

--- src/main/java/com/codingdojo/cynthia/controladores/Controlador.java
+++ src/main/java/com/codingdojo/cynthia/controladores/Controlador.java
@@ -1,6 +1,10 @@
 package com.codingdojo.cynthia.controladores;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,6 +13,19 @@ public class Controlador {
 	@GetMapping("/")
 	public String index() {
 		return "index.jsp";
+	}
+	
+	@GetMapping("/date")
+	public String date(Model model) {
+		
+		Date d = new Date();
+		SimpleDateFormat fecha = new SimpleDateFormat("E dd MMMM, yyyy");
+		String fecha_formateada = fecha.format(d);
+		
+		//model.addAttribute("fecha", fecha);
+		model.addAttribute("fecha", fecha_formateada);
+		
+		return "date.jsp";
 	}
 	
 }
